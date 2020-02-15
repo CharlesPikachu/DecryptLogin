a7357d312ba1b7542da6dd0430c936b2c8149208
CharlesPikachu <1159254961@qq.com>
2020-02-15 22:33:10 +0800

    process verification code in weibo according to #1 and #2

--- DecryptLogin/platforms/weibo.py
+++ DecryptLogin/platforms/weibo.py
@@ -10,7 +10,7 @@ Author:
 GitHub:
 	https://github.com/CharlesPikachu
 更新日期:
-	2020-01-28
+	2020-02-15
 '''
 import re
 import rsa
@@ -19,6 +19,7 @@ import random
 import base64
 import requests
 import warnings
+from ..utils.utils import *
 from binascii import b2a_hex
 warnings.filterwarnings('ignore')
 
@@ -40,6 +41,7 @@ Detail:
 class weibo():
 	def __init__(self, **kwargs):
 		self.info = 'weibo'
+		self.cur_path = os.getcwd()
 		self.session = requests.Session()
 	'''登录函数'''
 	def login(self, username, password, mode='mobile', crackvc_func=None, **kwargs):
@@ -63,61 +65,102 @@ class weibo():
 					'hfp': ''
 					}
 			res = self.session.post(self.login_url, headers=self.login_headers, data=data)
-			if res.json()['retcode'] == 20000000:
+			res_json = res.json()
+			# 登录成功
+			if res_json['retcode'] == 20000000:
 				print('[INFO]: Account -> %s, login successfully...' % username)
 				infos_return = {'username': username}
+				infos_return.update(res_json)
 				return infos_return, self.session
-			else:
+			# 用户名或密码错误
+			elif res_json['retcode'] == 50011002:
 				raise RuntimeError('Account -> %s, fail to login, username or password error...' % username)
+			# 其他错误
+			else:
+				raise RuntimeError(res_json['msg'])
 		elif mode == 'pc':
 			self.__initializePC()
-			# 请求prelogin_url
-			su = base64.b64encode(username.encode())
-			params = {
-						'entry': 'weibo',
-						'callback': 'sinaSSOController.preloginCallBack',
-						'su': su,
-						'rsakt': 'mod',
-						'checkpin': '1',
-						'client': 'ssologin.js(v1.4.19)',
-						'_': str(int(time.time()*1000))
-					}
-			res = self.session.get(self.prelogin_url, headers=self.headers, params=params, verify=False)
-			nonce = re.findall(r'"nonce":"(.*?)"', res.text)[0]
-			pubkey = re.findall(r'"pubkey":"(.*?)"', res.text)[0]
-			rsakv = re.findall(r'"rsakv":"(.*?)"', res.text)[0]
-			servertime = re.findall(r'"servertime":(.*?),', res.text)[0]
-			# 请求ssologin_url
-			publickey = rsa.PublicKey(int(pubkey, 16), int('10001', 16))
-			sp = rsa.encrypt((str(servertime)+'\t'+nonce+'\n'+password).encode(), publickey)
-			sp = b2a_hex(sp)
-			data_post = {
+			# 进行模拟登录
+			add_verification_code = False
+			while True:
+				# --是否需要验证码
+				if add_verification_code:
+					params = {
+								'r': str(int(random.random()*100000000)),
+								's': '0'
+							}
+					res = self.session.get(self.pin_url, headers=self.headers, params=params)
+					saveImage(res.content, os.path.join(self.cur_path, 'captcha.jpg'))
+					if crackvc_func is None:
+						showImage(os.path.join(self.cur_path, 'captcha.jpg'))
+						captcha = input('Input the Verification Code:')
+					else:
+						captcha = crackvc_func(os.path.join(self.cur_path, 'captcha.jpg'))
+					removeImage(os.path.join(self.cur_path, 'captcha.jpg'))
+				# --请求prelogin_url
+				su = base64.b64encode(username.encode())
+				params = {
 							'entry': 'weibo',
-							'gateway': '1',
-							'from': '',
-							'savestate': '7',
-							'qrcode_flag': 'false',
-							'useticket': '1',
-							'pagerefer': 'https://login.sina.com.cn/crossdomain2.php?action=logout&r=https%3A%2F%2Fweibo.com%2Flogout.php%3Fbackurl%3D%252F',
-							'vsnf': '1',
 							'su': su,
-							'service': 'miniblog',
-							'servertime': str(int(servertime)+random.randint(1, 20)),
-							'nonce': nonce,
-							'pwencode': 'rsa2',
-							'rsakv': rsakv,
-							'sp': sp,
-							'sr': '1536 * 864',
-							'encoding': 'UTF - 8',
-							'prelt': '35',
-							'url': 'https://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
-							'returntype': 'META'
+							'rsakt': 'mod',
+							'checkpin': '1',
+							'client': 'ssologin.js(v1.4.19)',
+							'_': str(int(time.time()*1000))
 						}
-			res = self.session.post(self.ssologin_url, headers=self.headers, data=data_post, allow_redirects=False, verify=False)
-			redirect_url = re.findall(r'location.replace\("(.*?)"\);', res.text)[0]
-			res = self.session.get(redirect_url, headers=self.headers, allow_redirects=False, verify=False)
+				res = self.session.get(self.prelogin_url, headers=self.headers, params=params, verify=False)
+				res_json = res.json()
+				nonce = res_json.get('nonce', '')
+				pubkey = res_json.get('pubkey', '')
+				rsakv = res_json.get('rsakv', '')
+				servertime = res_json.get('servertime', '')
+				# --请求ssologin_url
+				publickey = rsa.PublicKey(int(pubkey, 16), int('10001', 16))
+				sp = rsa.encrypt((str(servertime)+'\t'+nonce+'\n'+password).encode(), publickey)
+				sp = b2a_hex(sp)
+				data_post = {
+								'entry': 'account',
+								'gateway': '1',
+								'from': '',
+								'savestate': '30',
+								'useticket': '0',
+								'useticket': '1',
+								'pagerefer': '',
+								'vsnf': '1',
+								'su': su,
+								'service': 'account',
+								'servertime': str(int(servertime)+random.randint(1, 20)),
+								'nonce': nonce,
+								'pwencode': 'rsa2',
+								'rsakv': rsakv,
+								'sp': sp,
+								'sr': '1536 * 864',
+								'encoding': 'UTF - 8',
+								'cdult': '3',
+								'domain': 'sina.com.cn',
+								'prelt': '95',
+								'returntype': 'TEXT'
+							}
+				if add_verification_code:
+					data_post['door'] = captcha
+				res = self.session.post(self.ssologin_url, headers=self.headers, data=data_post, allow_redirects=False, verify=False)
+				res_json = res.json()
+				# --登录成功
+				if res_json['retcode'] == '0':
+					break
+				# --用户名或密码错误
+				elif res_json['retcode'] == '101':
+					raise RuntimeError('Account -> %s, fail to login, username or password error...' % username)
+				# --验证码错误
+				elif res_json['retcode'] == '2070':
+					raise RuntimeError('Account -> %s, fail to login, crack captcha error...' % username)
+				# --需要验证码
+				elif res_json['retcode'] == '4049':
+					add_verification_code = True
+				# --其他错误
+				else:
+					raise RuntimeError(res_json.get('reason', ''))
 			ticket, ssosavestate = re.findall(r'ticket=(.*?)&ssosavestate=(.*?)"', res.text)[0]
-			# 请求login_url
+			# 请求login_url和home_url, 进一步验证登录是否成功
 			params = {
 						'ticket': ticket,
 						'ssosavestate': str(ssosavestate),
@@ -133,9 +176,10 @@ class weibo():
 			if '我的首页' in res.text:
 				print('[INFO]: Account -> %s, login successfully...' % username)
 				infos_return = {'username': username}
+				infos_return.update(res_json)
 				return infos_return, self.session
 			else:
-				raise RuntimeError('Account -> %s, fail to login, username or password error...' % username)
+				raise RuntimeError('Account -> %s, fail to login, visit %s error...' % (username, self.home_url))
 		else:
 			raise ValueError('Unsupport argument in weibo.login -> mode %s, expect <mobile> or <pc>...' % mode)
 	'''初始化PC端'''
@@ -143,6 +187,7 @@ class weibo():
 		self.headers = {
 							'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 						}
+		self.pin_url = 'https://login.sina.com.cn/cgi/pin.php'
 		self.prelogin_url = 'https://login.sina.com.cn/sso/prelogin.php?'
 		self.ssologin_url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
 		self.login_url = 'https://passport.weibo.com/wbsso/login?'
