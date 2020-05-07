d82f308c9ae420d62b8202e4f800ec47b9159831
CharlesPikachu <1159254961@qq.com>
2020-05-07 19:38:05 +0800

    fix 12306

--- DecryptLogin/platforms/zt12306.py
+++ DecryptLogin/platforms/zt12306.py
@@ -10,7 +10,7 @@ Author:
 GitHub:
 	https://github.com/CharlesPikachu
 更新日期:
-	2020-02-29
+	2020-05-07
 '''
 import os
 import time
@@ -48,7 +48,7 @@ class zt12306():
 		# PC端接口
 		elif mode == 'pc':
 			self.__initializePC()
-			self.__downloadVcode()
+			self.__downloadCaptcha()
 			time.sleep(0.1)
 			res = self.__verifyCaptcha(crackvcFunc)
 			if not res:
@@ -68,9 +68,9 @@ class zt12306():
 		else:
 			raise ValueError('Unsupport argument in zt12306.login -> mode %s, expect <mobile> or <pc>...' % mode)
 	'''下载验证码'''
-	def __downloadVcode(self):
-		res = self.session.get(self.vcode_url, headers=self.headers)
-		saveImage(res.content, os.path.join(self.cur_path, 'vcode.jpg'))
+	def __downloadCaptcha(self):
+		res = self.session.get(self.captcha_url, headers=self.headers)
+		saveImage(res.content, os.path.join(self.cur_path, 'captcha.jpg'))
 		return True
 	'''验证码验证'''
 	def __verifyCaptcha(self, crackvcFunc):
@@ -78,21 +78,21 @@ class zt12306():
 		if crackvcFunc is None:
 			showImage(img_path)
 			user_enter = input('Enter the positions of captcha, use <,> to separate, such as <2,3>\n(From left to right, top to bottom -> 1,2,3,4,5,6,7,8):')
-			verify_list = []
+			digital_list = []
 			for each in user_enter.split(','):
 				each = each.strip()
 				try:
-					verify_list.append(self.positions[int(each)-1])
+					digital_list.append(self.positions[int(each)-1])
 				except:
 					raise RuntimeError('captcha format error...')
 		else:
-			verify_list = crackvcFunc(img_path)
+			digital_list = crackvcFunc(img_path)
 		data = {
-				'answer': ','.join(verify_list),
+				'answer': ','.join(digital_list),
 				'login_site': 'E',
 				'rand': 'sjrand'
 				}
-		res = self.session.post(url=self.verify_url, headers=self.headers, data=data)
+		res = self.session.post(url=self.captcha_check_url, headers=self.headers, data=data)
 		removeImage(img_path)
 		if res.json()['result_code'] == '4':
 			return True
@@ -104,8 +104,8 @@ class zt12306():
 							'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 						}
 		self.positions = ['36,46', '109,44', '181,47', '254,44', '33,112', '105,116', '186,116', '253,115']
-		self.vcode_url = 'https://kyfw.12306.cn/passport/captcha/captcha-image?login_site=E&module=login&rand=sjrand&0.5579044251920726'
-		self.verify_url = 'https://kyfw.12306.cn/passport/captcha/captcha-check'
+		self.captcha_url = 'https://kyfw.12306.cn/passport/captcha/captcha-image?login_site=E&module=login&rand=sjrand&0.5579044251920726'
+		self.captcha_check_url = 'https://kyfw.12306.cn/passport/captcha/captcha-check'
 		self.login_url = 'https://kyfw.12306.cn/passport/web/login'
 	'''初始化移动端'''
 	def __initializeMobile(self):
