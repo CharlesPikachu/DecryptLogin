8f8153150fc26a1b0d12b884ae33bbe33c9b2d22
CharlesPikachu <1159254961@qq.com>
2021-12-27 12:10:22 +0800

    format code style

--- examples/QQReports/generate.py
+++ examples/QQReports/generate.py
@@ -1,10 +1,10 @@
 '''
 Function:
-	QQ个人专属报告生成
+    QQ个人专属报告生成
 Author:
-	Charles
+    Charles
 微信公众号:
-	Charles的皮卡丘
+    Charles的皮卡丘
 '''
 import warnings
 from modules.utils import *
@@ -27,20 +27,20 @@ checkDir(RESULTS_SAVE_DIR)
 
 '''下载所有需要的数据'''
 def getReportsData():
-	qq_reports = QQReports(DATA_SAVE_DIR)
-	# 个人基本信息
-	personal_info = qq_reports.getPersonalInfo(PERSONAL_INFO_FILENAME+'.pkl')
-	makePersonalCard(personal_info, PERSONAL_INFO_BG_PATH, savepath=os.path.join(RESULTS_SAVE_DIR, PERSONAL_INFO_FILENAME+'.jpg'), fontpath=FONT_PATH)
-	# 好友信息
-	friends_info = qq_reports.getQQFriendsInfo(FRIENDS_INFO_FILENAME+'.pkl')
-	nicknames = [value[0] for value in friends_info.values()]
-	nicknames = dict(zip(nicknames, [1]*len(nicknames)))
-	drawWordCloud(nicknames, savepath=os.path.join(RESULTS_SAVE_DIR, FRIENDS_INFO_FILENAME+'.jpg'), font_path=FONT_PATH)
-	# 最近操作信息
-	recent_operation_info = qq_reports.getRecentOperationsInfo(RECENT_OPERATIONS_INFO_FILENAME+'.pkl')
-	makeRecentCard(friends_info, recent_operation_info, RECENT_OPERATIONS_INFO_BG_PATH, savepath=os.path.join(RESULTS_SAVE_DIR, RECENT_OPERATIONS_INFO_FILENAME+'.jpg'), fontpath=FONT_PATH)
+    qq_reports = QQReports(DATA_SAVE_DIR)
+    # 个人基本信息
+    personal_info = qq_reports.getPersonalInfo(PERSONAL_INFO_FILENAME+'.pkl')
+    makePersonalCard(personal_info, PERSONAL_INFO_BG_PATH, savepath=os.path.join(RESULTS_SAVE_DIR, PERSONAL_INFO_FILENAME+'.jpg'), fontpath=FONT_PATH)
+    # 好友信息
+    friends_info = qq_reports.getQQFriendsInfo(FRIENDS_INFO_FILENAME+'.pkl')
+    nicknames = [value[0] for value in friends_info.values()]
+    nicknames = dict(zip(nicknames, [1]*len(nicknames)))
+    drawWordCloud(nicknames, savepath=os.path.join(RESULTS_SAVE_DIR, FRIENDS_INFO_FILENAME+'.jpg'), font_path=FONT_PATH)
+    # 最近操作信息
+    recent_operation_info = qq_reports.getRecentOperationsInfo(RECENT_OPERATIONS_INFO_FILENAME+'.pkl')
+    makeRecentCard(friends_info, recent_operation_info, RECENT_OPERATIONS_INFO_BG_PATH, savepath=os.path.join(RESULTS_SAVE_DIR, RECENT_OPERATIONS_INFO_FILENAME+'.jpg'), fontpath=FONT_PATH)
 
 
 '''run'''
 if __name__ == '__main__':
-	getReportsData()
+    getReportsData()
