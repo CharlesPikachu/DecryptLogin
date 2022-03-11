303eb4d6ed75354399aa441d5c0683ed02aea322
CharlesPikachu <1159254961@qq.com>
2022-03-11 17:54:33 +0800

    fix the problem in unitest

--- DecryptLogin/modules/__init__.py
+++ DecryptLogin/modules/__init__.py
@@ -1,4 +1,17 @@
 '''initialize'''
-from .core import *
-from .utils import *
-from .clients import *
+from .core import (
+    douban, github, weibo, music163, zt12306, QQZone, QQQun, QQId, zhihu, bilibili, 
+    toutiao, taobao, jingdong, ifeng, sohu, zgconline, lagou, twitter, eSurfing, 
+    renren, w3cschool, fishc, youdao, baidupan, stackoverflow, codalab, pypi, douyu, 
+    migu, qunar, mieshop, mpweixin, baidutieba, dazhongdianping, jianguoyun, cloud189, qqmusic,
+    ximalaya, icourse163, xiaomihealth,
+)
+from .clients import (
+    MiguClient, PyPiClient, SohuClient, QQIdClient, QunarClient, FishCClient, LagouClient, IfengClient,
+    ZhihuClient, QQQunClient, WeiboClient, DouyuClient, YoudaoClient, RenRenClient, TaobaoClient,
+    GithubClient, DoubanClient, QQZoneClient, CodaLabClient, ToutiaoClient, TwitterClient, Zt12306Client,
+    MieShopClient, QQMusicClient, MpweixinClient, Cloud189Client, XimalayaClient, eSurfingClient,
+    Music163Client, BiliBiliClient, JingdongClient, BaiduPanClient, ZgconlineClient, W3CSchoolClient,
+    BaiduTiebaClient, Icourse163Client, JianguoyunClient, XiaomiHealthClient, StackoverflowClient,
+    DazhongdianpingClient,
+)
