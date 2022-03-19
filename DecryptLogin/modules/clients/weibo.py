d9f45614e39b4eab2179ca6024a2ce1a11dcaace
CharlesPikachu <1159254961@qq.com>
2022-03-19 17:15:57 +0800

    bump to v0.3.2, fix the bug in weibo client

--- DecryptLogin/modules/clients/weibo.py
+++ DecryptLogin/modules/clients/weibo.py
@@ -6,8 +6,9 @@ Author:
 微信公众号:
     Charles的皮卡丘
 更新日期:
-    2022-03-11
+    2022-03-19
 '''
+import re
 from .baseclient import BaseClient
 
 
@@ -17,8 +18,15 @@ class WeiboClient(BaseClient):
         super(WeiboClient, self).__init__(website_name='weibo', reload_history=reload_history, **kwargs)
     '''检查会话是否已经过期, 过期返回True'''
     def checksessionstatus(self, session, infos_return):
-        url = 'http://weibo.com/'
-        response = session.get(url)
-        if infos_return['nick'] in response.text:
-            return False
-        return True
+        if 'nick' in infos_return:
+            url = 'http://weibo.com/'
+            response = session.get(url)
+            if infos_return['nick'] in response.text:
+                return False
+            return True
+        else:
+            url = 'https://m.weibo.cn/u/7518276693?uid=7518276693&luicode=10000011&lfid=231093_-_selffollowed'
+            session.get(url)
+            if len(re.findall(r'fid%3D(\d+)%26', str(session.cookies))) > 0:
+                return False
+            return True
